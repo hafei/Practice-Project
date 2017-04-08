2cfb9ae6bd320f23e94d7f66dd276e25c8771b04
sean <zhangmengcoding@gmail.com>
2017-04-08 13:54:54 +0800

    0408

---
+++ 0408/app.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+
+
+# count = 2425
+url = 'http://jandan.net/ooxx/page-'
+import os
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+
+def spider(url, count):
+    res = requests.get(url + str(count))
+    html = BeautifulSoup(res.text)
+    # print(html)
+    for index, item in enumerate(html.select('#comments img')):
+        # print(item.attrs['src'])
+        # print('http://' + item.attrs['src'][2:])
+        if (not os.path.exists(str(count))):
+            os.mkdir(str(count))
+        filename = str(count) + '/{}.jpg'.format(index)
+        print(filename)
+        with open(filename, 'wb') as jpg:
+            jpg.write(requests.get('http://' + item.attrs['src'][2:], stream=True).content)
+
+
+def run(url, count):
+    while count > 0:
+        spider(url, count)
+        count = count - 1
+        time.sleep(random.randint(1,5))
+
+
+url = 'http://jandan.net/ooxx/page-'
+count = 2425
+
+run(url, count)
