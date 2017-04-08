f4b43d968153c24f77de36922336b7b2070aa510
sean <zhangmengcoding@gmail.com>
2017-04-08 19:08:14 +0800

    fix bug

---
+++ OO/MultipleInheritance.py
@@ -0,0 +1,46 @@
+class Animal(object):
+    pass
+
+
+# 行为
+class RunnableMixIn(object):
+    def run(self):
+        print('Running...')
+
+
+class FlyableMixIn(object):
+    def fly(self):
+        print('Flying...')
+
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+
+class Bird(Animal):
+    pass
+
+
+# 各种动物:
+class Dog(Mammal, RunnableMixIn):
+    pass
+
+
+class Bat(Mammal, FlyableMixIn):
+    pass
+
+
+class Parrot(Bird):
+    pass
+
+
+class Ostrich(Bird):
+    pass
+
+
+dog = Dog()
+dog.run()
+
+bat = Bat()
+bat.fly()
